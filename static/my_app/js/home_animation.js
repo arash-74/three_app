05741d47ccadb2c69f0387c3ddbe14ba21e086bf
arash <arash@gmail.com>
2026-06-22 13:04:42 +0330

    inti commit

---
+++ static/my_app/js/home_animation.js
@@ -0,0 +1,116 @@
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger)
+//preload animation
+let preloadAnimation = gsap.to(
+    '.preloader circle', {
+        rotate: 360,
+        duration: 1.5,
+        transformOrigin: 'center',
+        repeat: -1,
+        ease: "none"
+    }
+)
+
+export function stopPreloadAnimate() {
+    gsap.to('.preloader', {
+        duration: 1.5,
+        delay: 1.5,
+        autoAlpha: 0,
+        ease: "power4",
+
+    })
+}
+
+//preload camera animation
+export function startPreloadCameraAnimate(camera, cameraTarget) {
+    gsap.timeline()
+        .to(camera.position, {
+            x: -1.61,
+            y: 5.91,
+            z: 9.85,
+            duration: 1,
+            delay: 2,
+        })
+}
+
+//section 2 animation
+let s2Tl = gsap.timeline()
+let s2ST = ScrollTrigger.create({
+    trigger: '.section-2',
+    animation: s2Tl,
+    start: "top 90%",
+    end: "bottom bottom",
+    scrub: true,
+
+})
+// (12,1.75,-5)
+// (-0.48,1.65,0.428)
+export function section2Animate(camera, cameraTarget, car) {
+    s2Tl.to(camera.position, {
+        x: 13,
+        duration: 1
+    },).to(camera.position, {
+        x: 11.21,
+        y: 2.31,
+        z: -6.25,
+        duration: 1.5
+    }, '-=0.3',).to('.section-2 .container', {
+        x: 0,
+        duration: 2,
+    }, '+=0.2').to('.section-2 .container p:nth-child(1)', {
+        opacity: 1,
+    }).to('.section-2 .container p:nth-child(2)', {
+        opacity: 1,
+    }).to('.section-2 .container p:nth-child(3)', {
+        opacity: 1,
+    })
+}
+
+export function section3Animate(camera, car, cameraTarget) {
+    let carMaterials = []
+    let colorObj = {value: "#254567FF"};
+
+    car.traverse((child) => {
+        if (child.isMesh && child.material) {
+
+
+            if (Array.isArray(child.material)) {
+                child.material.forEach(mat => {
+                    if (mat.name === "M_Paint") {
+                        carMaterials.push(mat);
+                    }
+                });
+            } else {
+                if (child.material.name === "M_Paint") {
+                    carMaterials.push(child.material);
+                    colorObj.value = child.material.color.clone()
+                }
+            }
+        }
+    });
+
+    let animate = gsap.timeline()
+    ScrollTrigger.create({
+        trigger: '.section-3',
+        animation: animate,
+        start: "top 90%",
+        end: "bottom bottom",
+        scrub: true,
+    })
+    animate.to('.section-2 .container', {
+        x: "110%"
+    }).to(camera.position, {
+        x: -13.86,
+        y: 2.36,
+        z: -4.52
+    }, "-=0.5").to(colorObj, {
+        value: "#0000ff",
+        onUpdate: () => {
+            carMaterials.forEach(mat => {
+                mat.color.set(colorObj.value);
+            })
+        }
+    })
+}
---
+++ static/my_app/js/home_three.js
@@ -0,0 +1,115 @@
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
+import {HDRLoader} from "three/addons/loaders/HDRLoader.js";
+import {GUI} from 'lil-gui'
+import * as gsap from './home_animation.js'
+import {section3Animate} from "./home_animation.js";
+// define elements
+const canvas = document.getElementById('canvas');
+let size = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+let cameraTarget = new THREE.Vector3(0, 0, 0)
+let car
+
+
+// define scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#f5f5f5')
+
+//define helper
+// const axisHelper = new THREE.AxesHelper();
+// scene.add(axisHelper);
+
+//define camera
+export const camera = new THREE.PerspectiveCamera(50, size.width / size.height, 1, 1000);
+scene.add(camera);
+camera.lookAt(cameraTarget);
+camera.position.set(5, 12, 16)
+camera.rotation.set(-0.254, -0.11, 0)
+let cameraHelper = new OrbitControls(camera, canvas);
+cameraHelper.enabled = false
+//define env
+const envLoader = new HDRLoader();
+envLoader.load(environPath, envMap => {
+    envMap.mapping = THREE.EquirectangularReflectionMapping
+    scene.environment = envMap;
+})
+
+// define meshes
+const plan = new THREE.Mesh(
+    new THREE.PlaneGeometry(35, 15),
+    new THREE.MeshBasicMaterial({color: '#d9d9d9'}),
+);
+plan.rotation.x = -Math.PI / 2;
+scene.add(plan);
+// GLTF model
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load(modelPath, gltf => {
+    car = gltf.scene
+    scene.add(car);
+    car.scale.set(0.05, 0.05, 0.05);
+    car.position.set(0, 2, 3);
+    car.rotation.set(-0.02, 2, -0.03);
+    // carPosition.add(car.position, 'x').name('x')
+    // carPosition.add(car.position, 'y').name('y')
+    // carPosition.add(car.position, 'z').name('z')
+    // carRotate.add(car.rotation, 'x', -2, 2, 0.01).name('x')
+    // carRotate.add(car.rotation, 'y', -2, 2, 0.01).name('y')
+    // carRotate.add(car.rotation, 'z', -2, 2, 0.01).name('z')
+    gsap.stopPreloadAnimate()
+    gsap.startPreloadCameraAnimate(camera, cameraTarget)
+    gsap.section2Animate(camera, cameraTarget, car)
+    gsap.section3Animate(camera, car, cameraTarget)
+})
+// define gui
+const gui = new GUI()
+gui.hide()
+const position = gui.addFolder('camera position');
+const rotation = gui.addFolder('camera rotation');
+const carPosition = gui.addFolder('car position');
+const carRotate = gui.addFolder('car rotate');
+// define renderer
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setSize(size.width, size.height);
+renderer.render(scene, camera);
+
+// define animation frame
+function animate() {
+    requestAnimationFrame(animate);
+    // camera.lookAt(cameraTarget);
+    cameraHelper.update()
+    renderer.render(scene, camera);
+}
+
+animate();
+
+// define events
+window.addEventListener('resize', () => {
+    size.width = window.innerWidth;
+    size.height = window.innerHeight;
+    renderer.setSize(size.width, size.height);
+    camera.aspect = size.width / size.height;
+    camera.updateProjectionMatrix();
+    renderer.render(scene, camera);
+});
+// window.addEventListener('load', () => {
+//     window.scrollTo(0, 0);
+// });
+// gui
+let cameraOpt = {
+    look: false
+}
+
+const positionFolder = gui.addFolder('camera position');
+
+function myOnChange() {
+    camera.lookAt(car.position);
+}
+
+positionFolder.add(camera.position, 'x', -20, 20, 0.01)
+positionFolder.add(camera.position, 'y', -20, 20, 0.01)
+positionFolder.add(camera.position, 'z', -20, 20, 0.01)
